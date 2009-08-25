55d5ead2158fea16c9669482225306dcaf313e95
David Masover <ninja@slaphack.com>
2009-08-25 16:09:05 -0500

    Running at jQuery time, not at document end.
    This may turn out to be the same thing. I'll have to ask someone.

--- content.js
+++ content.js
@@ -1,11 +1,13 @@
-var port = chrome.extension.connect();
-window.collected = [];
-port.onMessage.addListener(function(message) {
-  $.each(message.rows, function() {
-    // jQuery each barfs on strings
-    for (var i in this.value) {
-      $(this.value[i]).remove();
-    }
+jQuery(function($) {
+  var port = chrome.extension.connect();
+  window.collected = [];
+  port.onMessage.addListener(function(message) {
+    $.each(message.rows, function() {
+      // jQuery each barfs on strings
+      for (var i in this.value) {
+        $(this.value[i]).remove();
+      }
+    });
   });
+  port.postMessage({domain: window.location.host});
 });
-port.postMessage({domain: window.location.host});
