eddc3fb8726500eb42a4690d100147bc536be545
David Masover <ninja@slaphack.com>
2009-08-24 22:48:08 -0500

    Bumped version number and switched to couchdb.

--- adquery.js
+++ adquery.js
@@ -1,2 +1,11 @@
-$('#ad-leaderboard').remove();
-$('#box-ad').remove();
+var domain_parts = window.location.href.split('.');
+var view_url = 'http://localhost:5984/adquery/_design/queries/_view/queries_by_domain';
+while(domain_parts.length > 0) {
+  $.get(view_url,
+    {key: domain_parts.join('.')},
+    function (data) {
+      alert(data);
+    }
+  );
+  domain_parts.shift();
+}
